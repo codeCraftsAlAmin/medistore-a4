9c7b16c0604a33311a3647f1cb1f4fd7d38c7dc7
Code Crafts <codecraftsman12@gmail.com>
2026-02-05 21:21:04 +0600

    try to solve cookie issue

--- medistore-backend/src/app.ts
+++ medistore-backend/src/app.ts
@@ -20,8 +20,9 @@ app.use(
   cors({
     origin: config.urls.frontend_url,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    exposedHeaders: ["Set-Cookie"],
   }),
 );
 
